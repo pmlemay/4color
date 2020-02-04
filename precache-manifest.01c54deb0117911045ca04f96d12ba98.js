e9c5955d42cfea06d31b9e5fa8a117755366150e
Pier-Michael Lemay <pier-michael.lemay@datahex.ca>
2020-02-04 17:38:49 -0500

    Updates

---
+++ precache-manifest.01c54deb0117911045ca04f96d12ba98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1039e3c666cebd2151b7e87c833f5864",
+    "url": "/4color/index.html"
+  },
+  {
+    "revision": "4493788a5d8dabb05a9f",
+    "url": "/4color/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "11d0067326f7df01a20d",
+    "url": "/4color/static/js/2.105f522f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/4color/static/js/2.105f522f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4493788a5d8dabb05a9f",
+    "url": "/4color/static/js/main.b39a4589.chunk.js"
+  },
+  {
+    "revision": "23bb1889938ac3e15d04",
+    "url": "/4color/static/js/runtime-main.7969f745.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/4color/static/media/logo.5d5d9eef.svg"
+  }
+]);
